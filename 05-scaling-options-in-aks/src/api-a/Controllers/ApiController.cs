da8290031d7e422531d108308b00c01372d06257
Evgeny Borzenin <evgeny.borzenin@gmail.com>
2022-01-10 23:13:02 +0100

    Add api-a source and k8s deployment

---
+++ 05-scaling-options-in-aks/src/api-a/Controllers/ApiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api_a.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("[api-a] - OK.");
+            return Ok("[api-a] - OK.");
+        }
+
+        [HttpGet("highcpu")]
+        // readiness/unstable
+        public IActionResult HighCpu()
+        {
+            var x = 0.0001;
+
+            for (var i = 0; i < 2000000; i++)
+            {
+                x += Math.Sqrt(x);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ 05-scaling-options-in-aks/src/api-a/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api_a.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+        
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Healthy()
+        {
+            return Ok("[api-a] - healthy");
+        }
+    }
+}
